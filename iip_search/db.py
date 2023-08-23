442bbc5233a16205d367cc9d8b5aca522d5073d1
Charles Pletcher <pletcher@users.noreply.github.com>
2023-08-23 10:56:15 -0400

    Don't automatically run ingestion on deployment

--- iip_search/db.py
+++ iip_search/db.py
@@ -9,7 +9,12 @@ DB_URL = os.getenv(
     "DB_URL", "postgresql+psycopg://postgres:postgres@localhost:5432/iip_search_dev"
 )
 
-engine = create_engine(DB_URL)
+engine = create_engine(DB_URL, 
+    pool_size=10,
+    max_overflow=2,
+    pool_recycle=300,
+    pool_use_lifo=True)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
--- start.sh
+++ start.sh
@@ -2,6 +2,4 @@
 
 alembic upgrade head
 
-python ingest_inscriptions.py &
-
 uvicorn iip_search.app:app --proxy-headers --host 0.0.0.0 --port 8080
